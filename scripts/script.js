32d497cdce413315e277fa03cebc87e164579414
Paola Badilla <paob.a@hotmail>
2021-06-09 15:43:04 -0600

    commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function toggleSections(){
+    var textoOculto = document.getElementById("textoOculto");
+    if (!textoOculto.style.display || textoOculto.style.display === "block") {
+        textoOculto.style.display = "none";
+        document.getElementById("toggleBloque").textContent = "Leer mas";
+    } else {
+        textoOculto.style.display = "block";
+        document.getElementById("toggleBloque").textContent = "Leer menos";
+    }
+}
